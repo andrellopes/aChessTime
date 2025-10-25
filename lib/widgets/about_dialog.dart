063fef23c3f5d6676c2b8885f6247779f5ac5106
allc <allc.dev@hotmail.com>
2025-10-25 10:51:34 -0300

    Update about dialog and pubspec

--- lib/widgets/about_dialog.dart
+++ lib/widgets/about_dialog.dart
@@ -56,7 +56,7 @@ void showAppAboutDialog(BuildContext context) {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   buildContactButton(Icons.code, 'GitHub',
-                      () => _launchURL('https://github.com/andrellopes')),
+                      () => _launchURL('https://github.com/andrellopes/aChessTime')),
                   buildContactButton(Icons.email_outlined, 'Email',
                       () => _launchURL('mailto:allc.dev@hotmail.com')),
                   buildContactButton(Icons.chat_bubble_outline, 'WhatsApp',
